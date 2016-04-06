0999bc2c3dccb126330c92438f00b75da4e3f560
Adrian <suvroc@o2.pl>
2016-04-06 22:16:39 +0200

    Add Doodle test

--- SeleniumWebDriver.Examples/Helpers/WebDriverExtensions.cs
+++ SeleniumWebDriver.Examples/Helpers/WebDriverExtensions.cs
@@ -21,7 +21,7 @@ namespace SeleniumWebDriver.Examples.Helpers
         public static void TakeScreenshot(this IWebDriver chromeDriver, string testName)
         {
             OpenQA.Selenium.Support.Extensions.WebDriverExtensions.TakeScreenshot(chromeDriver)
-                .SaveAsFile(string.Format("{0}_{1:yyyy-MM-dd-hh-mm-ss}.acceptance-exception.png", testName, DateTime.Now),
+                .SaveAsFile("C:/tmp/" + string.Format("{0}_{1:yyyy-MM-dd-hh-mm-ss}.acceptance-exception.png", testName, DateTime.Now),
                     ImageFormat.Png);
         }
     }
---
+++ SeleniumWebDriver.Examples/WebDriverDoodleTest.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.IE;
+using System;
+using System.Threading;
+using System.Linq;
+using SeleniumWebDriver.Examples.Helpers;
+using OpenQA.Selenium.Interactions;
+using System.IO;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework.Interfaces;
+
+namespace SeleniumWebDriver.Examples
+{
+    [TestFixture]
+    public class WebDriverDoodleTest
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Initalize()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var state = TestContext.CurrentContext.Result.Outcome;
+            if (state == ResultState.Error || state == ResultState.Failure)
+            {
+                _driver.TakeScreenshot(TestContext.CurrentContext.Test.FullName);
+            }
+
+            _driver.Quit();
+        }
+
+        [Test]
+        public void ShouldCreateDoodle()
+        {
+            _driver.Navigate().GoToUrl("http://doodle.com/en_GB/");
+
+            var scheduleEventButton = _driver.FindElement(
+                By.CssSelector("#doodleExample > div.wizardOrExample.spaceBBefore > a"));
+            scheduleEventButton.Click();
+            Assert.AreEqual(_driver.Url, "http://doodle.com/create");
+
+
+            _driver.FindElement(By.Id("title"))
+                .SendKeys("Diwebsity test doodle");
+            _driver.FindElement(By.Id("initiatorAlias"))
+                .SendKeys("Diwebsity tester");
+            _driver.FindElement(By.Id("initiatorEmail"))
+                .SendKeys("seleniumTester@diwebsity.com");
+
+            _driver.FindElement(By.Id("next1"))
+                .Click();
+
+            var dateId = "cell" + DateTime.Now.ToString("yyyyMMdd");
+            var waitDriver = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            waitDriver.Until(
+                ExpectedConditions.ElementToBeClickable(By.Id(dateId)));
+            _driver.FindElement(By.Id(dateId))
+                .Click();
+            _driver.FindElement(By.Id("next2a"))
+                .Click();
+
+            _driver.FindElement(By.Id("do0_0"))
+                .SendKeys("12:00");
+            _driver.FindElement(By.Id("do0_1"))
+                .SendKeys("13:00");
+            _driver.FindElement(By.Id("do0_2"))
+                .SendKeys("14:00");
+            _driver.FindElement(By.Id("next2b"))
+                .Click();
+
+            waitDriver.Until(
+                ExpectedConditions.ElementToBeClickable(By.Id("next3s")));
+            _driver.FindElement(By.Id("next3s"))
+                .Click();
+
+            waitDriver.Until(
+                ExpectedConditions.ElementToBeClickable(By.Id("finish4a")));
+            Thread.Sleep(1000);
+            _driver.FindElement(By.Id("finish4a"))
+                .Click();
+
+            waitDriver.Until(
+                ExpectedConditions.ElementToBeClickable(By.Id("participationLink")));
+            var surveyUrl = 
+                _driver.FindElement(By.Id("participtionLink")).Text;
+            _driver.Navigate().GoToUrl(surveyUrl);
+
+            Assert.AreEqual(_driver.FindElement(By.Id("pollTitle")).Text,
+                "Diwebsity test doodle");
+        }
+    }
+}
