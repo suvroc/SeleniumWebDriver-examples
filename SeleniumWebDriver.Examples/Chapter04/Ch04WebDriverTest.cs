4354058bad0f9f7d955095f7ac32e1812d0c5d5f
Adrian Sroka <suvroc@o2.pl>
2016-07-07 15:49:50 +0200

    Split into chapters

---
+++ SeleniumWebDriver.Examples/Chapter04/Ch04WebDriverTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriver.Examples.Chapter04
+{
+    [TestFixture]
+    public class Ch04WebDriverTest
+    {
+        [Test]
+        public void ShouldTranslateWord()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://translate.google.com/#en/pl");
+
+            var sourceInput = driver.FindElement(By.Id("source"));
+
+            sourceInput.SendKeys("cat");
+            sourceInput.SendKeys(Keys.Enter);
+
+            Thread.Sleep(500);
+
+            var resultInput = driver.FindElement(By.Id("result_box"));
+
+            Assert.AreEqual(resultInput.Text, "kot");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void TheTest()
+        {
+            var driver = new ChromeDriver();
+            var baseURL = "https://translate.google.com";
+
+            driver.Navigate().GoToUrl(baseURL + "/#en/pl/cat");
+            driver.FindElement(By.Id("source")).Clear();
+            driver.FindElement(By.Id("source")).SendKeys("cat");
+            for (int second = 0; ; second++)
+            {
+                if (second >= 60) Assert.Fail("timeout");
+                try
+                {
+                    if ("kot" == driver.FindElement(By.Id("result_box")).Text) break;
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+                Thread.Sleep(1000);
+            }
+            Assert.AreEqual("kot", driver.FindElement(By.Id("result_box")).Text);
+        }
+
+
+    }
+}
---
+++ SeleniumWebDriver.Examples/Chapter07/Ch07WebDriverTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumWebDriver.Examples.Helpers;
+
+namespace SeleniumWebDriver.Examples.Chapter07
+{
+    [TestFixture]
+    public class Ch07WebDriverTest
+    {
+        [Test]
+        public void ShouldSelectHousesList()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://commons.wikimedia.org/wiki/Category:Houses");
+
+            var houseTile = driver.FindElement(By.ClassName("gallerybox"));
+
+            var link = houseTile.FindElement(By.CssSelector(".gallerytext > a"));
+
+            link.Click();
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldSelectHousesListItems()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://commons.wikimedia.org/wiki/Category:Houses");
+
+            var houseTiles = driver.FindElements(By.ClassName("gallerybox"))
+                .Select(x => new ListItem(
+                    x.FindElement(By.CssSelector(".gallerytext > a")).Text, x));
+
+            var houseTile = houseTiles.Single(x => x.Name == "Buiding.jpg");
+
+            var link = houseTile.Element
+                .FindElement(By.ClassName("thumb"));
+
+            link.Click();
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldGetElementById()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://doodle.com/en_GB/");
+
+            
+
+            driver.Quit();
+        }
+    }
+}
